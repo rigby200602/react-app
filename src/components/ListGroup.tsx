d590c8ec0983bcc738478538313a0c3a9f28762b
rigby200602 <tungle3456@gmail.com>
2025-02-15 19:57:58 +0700

    tap su dung fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
+import { Fragment } from "react/jsx-runtime";
+
 function ListGroup() {
+    const item = ['Paris','San Francisco', 'New York', 'London','Tokyo']
+        
     return (
-    <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <Fragment>
+        <h1>List</h1>
+        <ul className="list-group">
+            {item.map(item=> <li>{item}</li>)}
+        </ul>
+    </Fragment>
     )
 }
 
