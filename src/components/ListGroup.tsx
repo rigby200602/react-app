f5585fc24ad1a89a9f62fab9b74141f7f3ddfff3
rigby200602 <tungle3456@gmail.com>
2025-02-16 22:09:50 +0700

    useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 function ListGroup() {
     const item = ['Paris','San Francisco', 'New York', 'London','Tokyo']
-        
+    const [selectedItem,setSelectedItem] = useState(-1)
     return (
     <>
         <h1>List</h1>
         <ul className="list-group">
-            {item.map((item, index)=> <li key={item} className="list-group-item" onClick={() => console.log(item, index)}>{item}</li>)}
+            {item.map((item, index)=> <li key={item} 
+            className={selectedItem === index ? "list-group-item active": "list-group-item"}
+            onClick={() => {setSelectedItem(index)}}>{item}</li>)}
         </ul>
     </>
     )
